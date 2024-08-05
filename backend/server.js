e69f9d565c2baba1c889c29811e675dd02328884
naufaalrmdhn <naufal.rmdhn77@gmail.com>
2024-08-05 15:26:45 +0700

    p37

--- backend/server.js
+++ backend/server.js
@@ -13,9 +13,6 @@ const usersFilePath = path.join(__dirname, 'users.json');
 
 // Helper function to read users file
 function readUsersFile() {
-    if (!fs.existsSync(usersFilePath)) {
-        fs.writeFileSync(usersFilePath, JSON.stringify({}, null, 2));
-    }
     return JSON.parse(fs.readFileSync(usersFilePath, 'utf8'));
 }
 
